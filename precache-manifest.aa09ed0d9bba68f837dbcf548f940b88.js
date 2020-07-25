683616812a43a08da08f129159ab9f377c066fda
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2020-07-25 13:42:15 +0300

    Updates

---
+++ precache-manifest.aa09ed0d9bba68f837dbcf548f940b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1e8ae2152530c76e1c49e7d743b628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1f97a10f13447254f73",
+    "url": "/static/css/main.71b9790b.chunk.css"
+  },
+  {
+    "revision": "2885d78280db6c307ece",
+    "url": "/static/js/2.507ae39b.chunk.js"
+  },
+  {
+    "revision": "f1f97a10f13447254f73",
+    "url": "/static/js/main.8df1f483.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "6c28dd091cc8a1aa6de136b46d6c2afd",
+    "url": "/static/media/me.6c28dd09.jpg"
+  }
+]);
